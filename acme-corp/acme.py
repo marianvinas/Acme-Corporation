7e31b2d9700cbf30c1036be3094917bf8c7aedbd
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-18 03:39:43 -0700

    Added Boxing Glove as a product

--- acme-corp/acme.py
+++ acme-corp/acme.py
@@ -5,7 +5,7 @@ class Product:
         # these are attributes
         self.name = name
         self.price = 10
-        self.weight = 20
+        self.weight = 10
         self.flammability = 0.5
         self.identifier = random.randint(1000000, 9999999)
 
@@ -21,23 +21,41 @@ class Product:
     def explode(self):
         explode = self.flammability * self.weight
         if explode < 10:
-            print(f"...fizzle.")
+            print(f"...it's a glove.")
         elif explode >= 50:
             print("...boom!")
         else:
             print("...it's a glove.")
 
+class BoxingGlove(Product):
+    def __init__(self, name, price, weight, glove, flammability=[], identifier=[]):
+        super().__init__(self, name, price, weight, flammability=[], identifier=[])
+        self.glove = glove
+
+    def punch(self):
+        punch = self.weight
+        if punch < 5:
+            print(f"That tickles.")
+        elif punch >= 5:
+            print(f"Hey that hurt!")
+        else:
+            print("OUCH!")
+
 if __name__ == "__main__":
     product1 = [
-        {"name": "A Cool Toy", "price": "10", "weight": "20", "flammability": "0.5"},
+        {"name": "A Cool Toy", "price": "10", "weight": "20", "flammability": "0.5", "glove": "10"},
     ]
-    #print(product1.name)
-    #print(product1.price)
-    #print(product1.weight)
-    #print(product1.flammability)
-    #print(product1.identifier)
+    
     for d in product1:
         product = Product(d["name"], d["price"], d["weight"], d["flammability"])
         print(product.name)
-        product.Stealability()
-        product.explode()
+        #product.Stealability()
+        #product.explode()
+
+    for d in product1:    
+        glove = Product(d["name"], d["price"], d["weight"], d["flammability"], d["glove"])
+        print(glove.name)
+        print(glove.price)
+        print(glove.weight)
+        #glove.punch()
+        glove.explode()
