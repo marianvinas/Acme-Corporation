9dc54e39c7a67401c4b16deb7f81b99224c64ef4
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-18 18:29:11 -0700

    added readme info

--- acme-corp/acme_test.py
+++ acme-corp/acme_test.py
@@ -8,7 +8,8 @@ class AcmeProductTests(unittest.TestCase):
     """Making sure Acme products are the tops!"""
     def test_default_product_price(self):
         """Test default product price being 10."""
-        prod = Product('Test Product')
+        prod = Product('Test Product'):
+                pass
         self.assertEqual(prod.price, 10)
 
     def test_default_product_weight(self):
