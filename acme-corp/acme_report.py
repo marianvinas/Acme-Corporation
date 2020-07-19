41bdac16977541dde7c7c0b82515a57d0eb02989
MarianVinas <marian-vinas@lambdastudents.com>
2020-07-18 17:41:07 -0700

    Acme testing

--- acme-corp/acme_report.py
+++ acme-corp/acme_report.py
@@ -1,30 +1,58 @@
 # !/usr/bin/env python
+
 from random import randint, sample, uniform
 from acme import Product
 
-class Product:
-    def __init__(self, name, price, weight, flammability, identifier=[]):
-        # these are attributes
-        self.name = name
-        self.price = 10
-        self.weight = 10
-        self.flammability = 0.5
-        self.identifier = random.randint(1000000, 9999999)
-​
-# Useful to use with random.sample to generate names
+
 ADJECTIVES = ['Awesome', 'Shiny', 'Impressive', 'Portable', 'Improved']
 NOUNS = ['Anvil', 'Catapult', 'Disguise', 'Mousetrap', '???']
-​
-​
+
 def generate_products(num_products=30):
-  products = [random.randint]
-  # TODO - your code! Generate and add random products.
-  return products
-​
-​
+    products = []
+    for i in range(num_products):
+        adjective = sample(ADJECTIVES, 1)[0]
+        noun = sample(NOUNS, 1)[0]
+        name = adjective + ' ' + noun
+        price = randint(5, 100)
+        weight = randint(5, 100)
+        flammability = uniform(0, 2.5)
+        prod = Product(name, price, weight, flammability)
+        products.append(prod)
+    return products
+
+
+    def generate_products(num_products=30):
+        products = []
+        for i in range(num_products):
+            adjective = sample(ADJECTIVES, 1)[0]
+            noun = sample(NOUNS, 1)[0]
+            name = adjective + ' ' + noun
+            price = randint(5, 100)
+            weight = randint(5, 100)
+            flammability = uniform(0, 2.5)
+            prod = Product(name, price, weight, flammability)
+            products.append(prod)
+        return products
+
+
 def inventory_report(products):
-  pass  # TODO - your code! Loop over the products to calculate the report.
-​
-​
+    name_list = []
+    weight_list = []
+    price_list = []
+    flammability_list = []
+    for product in products:
+        name_list.append(product.name)
+        weight_list.append(product.weight)
+        price_list.append(product.price)
+        flammability_list.append(product.flammability)
+    uniques = len(set(name_list))
+    mean_weight = sum(weight_list) / len(weight_list)
+    mean_price = sum(price_list) / len(price_list)
+    mean_flammability = sum(flammability_list) / len(flammability_list)
+    print(f"ACME INVENTORY REPORT Unique Products = {uniques} \n",
+           f"Average Price = {mean_price} \n",
+           f"Average Weight = {mean_weight} \n",
+           f"Average Flammability = {mean_flammability}")
+
 if __name__ == '__main__':
-  inventory_report(generate_products())
+    inventory_report(generate_products())
---
+++ test/acme_test.py
@@ -0,0 +1,36 @@
+import unittest
+#from acme import Product
+#from acme_report import generate_products, ADJECTIVES, NOUNS
+
+
+class AcmeProductTests(unittest.TestCase):
+    """Making sure Acme products are the tops!"""
+    def test_default_product_price(self):
+        """Test default product price being 10."""
+        prod = Product('Test Product')
+        self.assertEqual(prod.price, 10)
+
+    def test_default_product_weight(self):
+        prod = Product('Test Product')
+        self.assertEqual(prod.weight, 20)
+
+    def test_stealyboom(self):
+        prod = Product(name='Test Product', price=100, weight=100, flammability=100)
+        self.assertEqual(prod.stealability(), "Very stealable!")
+        self.assertEqual(prod.explode(), "...BABOOM!!")
+
+
+class AcmeReportTests(unittest.TestCase):
+    def test_default_num_products(self):
+        case = generate_products()
+        self.assertEqual(len(case), 30)
+
+    def test_legal_names(self):
+        case = generate_products()
+        for product in case:
+            adjective, noun = product.name.split()
+            self.assertIn(adjective, ADJECTIVES)
+            self.assertIn(noun, NOUNS)
+
+if __name__ == '__main__':
+    unittest.main()
